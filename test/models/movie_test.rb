9a11ab220fcca813153d885f89d0e791df125330
Mariel Van Norman <melvinopolis@gmail.com>
2015-09-08 23:19:40 -0400

    adds event model and more tests

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,9 +1,7 @@
 require 'test_helper'
 
 class MovieTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
   	should validate_presence_of(:title)
   	should validate_presence_of(:url)
 
